d35da92a826ce6ec8af2dc75d583af66d13a9321
Moustafa <moustafa.safwat@sumup.com>
2025-04-04 18:23:48 +0200

    schedule_interval

--- airflow/dags/hello.py
+++ airflow/dags/hello.py
@@ -3,7 +3,11 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
-with DAG(dag_id="tembi-data-pipeline", start_date=airflow.utils.dates.days_ago(14), schedule_interval=None) as dag:
+with DAG(
+    dag_id="tembi-data-pipeline", 
+    start_date=airflow.utils.dates.days_ago(14), 
+    schedule_interval="@daily"
+    ) as dag:
     # Tasks are represented as operators
     collect = BashOperator(task_id="collect-data", bash_command="echo collect")
 
