dd48e22fe49f21c2ffbc5848e4d751b518acb3ed
Moustafa <moustafa.safwat@sumup.com>
2025-04-05 01:40:31 +0200

    dag update

--- dags/handwritten_digits_classifier.py
+++ dags/handwritten_digits_classifier.py
@@ -25,3 +25,42 @@ download_mnist_data = S3CopyObjectOperator(
     aws_conn_id="aws-airflow-s3",
     dag=dag,
 )
+
+
+def _extract_mnist_data(): 
+    s3hook = S3Hook(aws_conn_id="aws-airflow-s3")
+
+    # Download S3 dataset into memory
+    mnist_buffer = io.BytesIO() 
+    mnist_obj = s3hook.get_key(
+       bucket_name="sagemaker-mnist-test-data2",
+       key="mnist.pkl.gz",
+    )
+    mnist_obj.download_fileobj(mnist_buffer)
+
+    # Unpack gzip file, extract dataset, convert, upload back to S3
+    mnist_buffer.seek(0)
+    with gzip.GzipFile(fileobj=mnist_buffer, mode="rb") as f:
+        train_set, _, _ = pickle.loads(f.read(), encoding="latin1")
+        output_buffer = io.BytesIO()
+        write_numpy_to_dense_tensor(
+            file=output_buffer,
+            array=train_set[0],
+            labels=train_set[1],
+        )
+        
+    output_buffer.seek(0) 
+    s3hook.load_file_obj (
+        output_buffer,
+        key="mnist_data",
+        bucket_name="sagemaker-mnist-test-data2",
+        replace=True,
+    )
+
+extract_mnist_data = PythonOperator( 
+    task_id="extract_mnist_data", 
+    python_callable=_extract_mnist_data, 
+    dag=dag,
+)
+
+download_mnist_data >> extract_mnist_data 
