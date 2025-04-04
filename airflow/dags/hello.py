fd24ebe708b40acb053f602d7dd53a7ec5f8032c
Moustafa <moustafa.safwat@sumup.com>
2025-04-04 18:18:06 +0200

    change

--- airflow/dags/hello.py
+++ airflow/dags/hello.py
@@ -5,11 +5,9 @@ from airflow.operators.bash import BashOperator
 
 with DAG(dag_id="tembi-data-pipeline", start_date=airflow.utils.dates.days_ago(14), schedule_interval=None) as dag:
     # Tasks are represented as operators
-    collect = BashOperator(task_id="collect-data", bash_command="echo hello")
+    collect = BashOperator(task_id="collect-data", bash_command="echo collect")
 
-    @task()
-    def transform():
-        print("transform")
+    transform = BashOperator(task_id="transform-data", bash_command="echo transform")
 
     # Set dependencies between tasks
     collect >> transform
