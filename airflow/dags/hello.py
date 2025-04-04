1b123a30a10de86adf8676681c7c1b6f34b1a88c
Moustafa <moustafa.safwat@sumup.com>
2025-04-04 18:07:59 +0200

    fix

--- airflow/dags/hello.py
+++ airflow/dags/hello.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 
 with DAG(dag_id="tembi-data-pipeline", start_date=datetime(2024, 8, 8), schedule="0 0 * * *") as dag:
     # Tasks are represented as operators
-    collect = BashOperator(task_id="Collect Data", bash_command="echo hello")
+    collect = BashOperator(task_id="collect-data", bash_command="echo hello")
 
     @task()
     def transform():
