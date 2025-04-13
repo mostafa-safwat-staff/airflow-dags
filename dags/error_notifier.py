7c8de188f0f1108ee463e951a55605127759729c
Moustafa Kotb <mostafa.safwat.staff@gmail.com>
2025-04-13 22:39:03 +0200

    add error email airflow

--- dags/error_notifier.py
+++ dags/error_notifier.py
@@ -1,12 +1,27 @@
 from airflow import DAG
+from airflow.operators.python import BashOperator
 import airflow.utils.dates
 
 def send_error():
-    raise "Error!"
+    print("ERROR!")
 
 dag = DAG (
     dag_id = "error_notifier",
     schedule_interval=None, 
     start_date=airflow.utils.dates.days_ago(3),
+    default_args={"on_failure_callback": send_error, "email": "mostafa.safwat.1@gmail.com"},
     on_failure_callback=send_error
 )
+
+failing_task = BashOperator(
+    task_id="failing_task",
+    bash_command="exit 1",
+    dag=dag,
+)
+
+# sleeptask = BashOperator(
+#     task_id="sleeptask",
+#     bash_command="sleep 60",
+#     sla=datetime.timedelta(minutes=2),
+#     dag=dag,
+# )
