ce5d215b2a239f454f72655567ddfabeb01b88da
Moustafa Kotb <mostafa.safwat.staff@gmail.com>
2025-04-15 04:18:48 +0200

    lint

--- dags/python_version_log.py
+++ dags/python_version_log.py
@@ -3,13 +3,17 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 import sys
 
+
 def log_python_version():
     print("Python version:", sys.version)
 
-with DAG("check_python_version",
-         start_date=datetime(2024, 1, 1),
-         schedule_interval=None,
-         catchup=False) as dag:
+
+with DAG(
+    "check_python_version",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
 
     check_version = PythonOperator(
         task_id="log_python_version",
